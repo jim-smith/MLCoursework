873c09df9a6dca086518f09f61f2010818df5f31
jim-smith <james.smith@uwe.ac.uk>
2017-02-06 16:52:49 +0000

    Initial Commit

---
+++ MLCoursework/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MLCoursework
+//
+//  Created by Jim Smith on 06/02/2017.
+//  Copyright © 2017 Jim SmithJim Smith. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
